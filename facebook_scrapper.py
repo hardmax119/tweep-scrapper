bd362e99cdd66bfb5599433ec4ffbd93cbe89f2b
Max <ndugatuda.maxwell@gmail.com>
2025-02-05 11:17:45 +0000

    add credentials to env

--- facebook_scrapper.py
+++ facebook_scrapper.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 import time
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
+EMAIL = os.getenv('email')
+PASSWORD = os.getenv('fbpassword')
 url = "https://www.facebook.com/NatWest?locale=en_GB"
 
 with sync_playwright() as p:
@@ -11,18 +15,21 @@ with sync_playwright() as p:
     page = browser.new_page()
     print(f"Visiting page: {url}")
     page.goto(url)
+    # page.
     time.sleep(5)  
 
     soup = BeautifulSoup(page.content(), 'html.parser')
-    
-    posts = soup.find_all("div", class_="content")
-    count = 0
-    for post in posts:
-        count += 1
-        post_content = post.find("div", class_="content")
-        print(f"Post count: {count}")
-        print(post)
-        if count > 20:
-            break
+    with open('facebook.txt', 'w') as f:
+        f.write(soup.get_text(separator='/n', strip=True))
+
+    # posts = soup.find_all("div", class_="content")
+    # count = 0
+    # for post in posts:
+    #     count += 1
+    #     post_content = post.find("div", class_="content")
+    #     print(f"Post count: {count}")
+    #     print(post)
+    #     if count > 20:
+    #         break
     browser.close()
     print('Broswer closed!!')
